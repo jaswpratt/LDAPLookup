37fcc8611bcfdf8cb6437fc56bf4a1546616cd0d
Lager <Lager@LAPTOP-K34LBBPM>
2021-01-10 18:36:48 -0500

    Initial commit

---
+++ src/main/java/tn/loudoun/lenoir/LDAPLookup/LdapLookupApplication.java
@@ -0,0 +1,13 @@
+package tn.loudoun.lenoir.LDAPLookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/loudoun/lenoir/LDAPLookup/LdapLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.loudoun.lenoir.LDAPLookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
